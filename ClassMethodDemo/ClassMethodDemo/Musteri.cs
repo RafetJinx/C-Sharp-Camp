a0ba0da18a0cfbc03e85d04b33a0c6e845a7cbb1
Rafet Ersoy <63067602+RafetJinx@users.noreply.github.com>
2021-07-02 19:34:06 +0300

    Week 3 (#2)
    * Week 3 - Homework 2 Completed
    
    Week 3 - Homework 2 Completed
    
    * Week 3 - Homework 3 Completed
    
    Week 3 - Homework 3 Completed

---
+++ ClassMethodDemo/ClassMethodDemo/Musteri.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodDemo
+{
+    class Musteri
+    {
+        public Musteri(int id, string name, string lastName, int age, int birthYear)
+        {
+            this.id = id;
+            this.name = name;
+            this.lastName = lastName;
+            this.age = age;
+            this.birthYear = birthYear;
+        }
+        public int id { get; set; }
+        public string name { get; set; }
+        public string lastName { get; set; }
+        public int age { get; set; }
+        public int birthYear { get; set; }
+
+        public override string ToString()
+        {
+            string expression = "Musteri Bilgileri:"
+                + "\nId:" + id
+                + "\nName: " + name
+                + "\nLast Name: " + lastName
+                + "\nAge: " + age
+                + "\nBirth Year: " + birthYear;
+            return expression;
+        }
+    }
+
+}
---
+++ ClassMethodDemo/ClassMethodDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ClassMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri(1,"Rafet","Ersoy",21,2000);
+            Musteri musteri2 = new Musteri(2,"Salih","SS",20,2001);
+            Musteri musteri3 = new Musteri(3,"Berkay","BB",21,2000);
+
+            Musteri[] musteriler = { musteri1, musteri2, musteri3 };
+
+            MusteriManager musteriManager = new MusteriManager();
+
+            musteriManager.MusteriEkle(musteri1);
+            Console.WriteLine();
+            musteriManager.MusteriEkle(musteri2);
+            Console.WriteLine();
+            musteriManager.MusteriEkle(musteri3);
+            Console.WriteLine();
+            
+            musteriManager.MusterileriListele(musteriler);
+
+            musteriManager.MusteriSil(musteri1);
+
+        }
+    }
+
+}
