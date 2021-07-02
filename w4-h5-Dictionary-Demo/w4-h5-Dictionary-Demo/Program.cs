c9902681fc650f4f1385294248176dff515e38a0
Rafet Ersoy <63067602+RafetJinx@users.noreply.github.com>
2021-07-02 23:51:16 +0300

    Week 4 - Homework 5 Completed (#3)
    Week 4 - Homework 5 Completed

---
+++ w4-h5-Dictionary-Demo/w4-h5-Dictionary-Demo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace w4_h5_Dictionary_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> students = new MyDictionary<int, string>();
+            students.Add(101,"Rafet");
+            students.Add(102, "Seda");
+            students.Add(103, "Berkay");
+            students.Add(104, "Salih");
+            students.Add(105, "Rafet");
+            students.Add(102, "Ahmet"); // Ogrencilere eklenmeyecektir ve ekrana bu bilgi yazdirilacaktır. Cunku aynı anahtara sahip baska bir ogrenci bulunmaktadir.
+        }
+    }
+}
