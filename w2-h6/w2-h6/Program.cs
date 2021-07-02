ee18f4080a6db105859153d59776960bc8ec4f2e
Rafet Ersoy <rafet.ersoy.cs@gmail.com>
2021-07-02 18:40:10 +0300

    Week 2 - Homework 6 Completed
    Week 2 - Homework 6 Completed

---
+++ w2-h6/w2-h6/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace w2_h6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product Rafet = new Product("rafet");
+            Product Berkay = new Product("berkay");
+            Product Seda = new Product("seda");
+            Product Salih = new Product("salih");
+
+            Product[] products = { Rafet, Berkay, Seda, Salih };
+
+            for (int i = 0; i < products.Length; i++)
+            {
+                Console.WriteLine(products[i].name);
+            }
+
+            foreach (Product product in products)
+            {
+                Console.WriteLine(product.name);
+            }
+
+            int j = products.Length - 1;
+
+            while (j >= 0)
+            {
+                Console.WriteLine(products[j].name);
+                j--;
+            }
+        }
+
+        class Product
+        {
+            public string name;
+            public Product(string name)
+            {
+                this.name = name;
+            }
+        }
+    }
+}
