fe7e3984b61a820bf50dda34894dad8f0e374191
Rafet Ersoy <rafet.ersoy.cs@gmail.com>
2021-07-02 19:32:10 +0300

    Week 3 - Homework 3 Completed
    Week 3 - Homework 3 Completed

---
+++ ClassMethodDemo/ClassMethodDemo/MusteriManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodDemo
+{
+    class MusteriManager
+    {
+        public void MusteriEkle(Musteri musteri)
+        {
+            Console.WriteLine("Musteri eklendi." + musteri.ToString());
+            
+        }
+
+        public void MusteriSil(Musteri musteri)
+        {
+            Console.WriteLine("Musteri silindi." + musteri.ToString());
+        }
+
+        public void MusterileriListele(Musteri[] musteriler)
+        {
+            foreach(Musteri musteri in musteriler)
+            {
+                Console.WriteLine(musteri);
+                Console.WriteLine("---------------------");
+            }
+        }
+    }
+}
